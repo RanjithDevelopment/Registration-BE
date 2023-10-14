61e590da078e2a6de31735840ddf76edf2fdaeba
RANJITH <73814077+RanjithDevelopment@users.noreply.github.com>
2023-10-14 20:18:39 +0530

    Add files via upload

--- modules/registerModule.js
+++ modules/registerModule.js
@@ -1,4 +1,5 @@
 const user = require("../models/userModels");
+const { ObjectId } = require('mongodb');
 
 module.exports.signup = async(req,res,next) =>{
     try{
@@ -15,3 +16,44 @@ res.status(200).send({message:"user creadted successfully"});
         console.log(err);
     }
 }
+
+module.exports.getUsers = async(req,res)=>{
+    try {
+        const allUsers = await user.find({});
+        res.status(200).send(allUsers) 
+    } catch (error) {
+        console.log(error)
+        res.status(400).send({msg:"can't able to fetch the Users Data"})
+    }
+
+}
+
+module.exports.updateUser = async(req,res,next) =>{
+    try {
+        const updated = await user.findByIdAndUpdate(req.params.id,{...req.body})
+        if(updated){
+        return res.status(200).json({ updated });
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(400).send({msg:"can't able to update the details"})
+    }
+} 
+
+module.exports.deleteUser = async(req,res)=>{
+    try {
+const isValid = ObjectId.isValid(req.params.id);
+       
+     if(isValid){
+        const remove = await user.deleteOne({ _id: new ObjectId(req.params.id) });
+     if(remove){
+        return res.status(200).json({ remove });
+     }
+    }
+     res.status(500).send({msg:"can't able to delete the user"})
+    } catch (error) {
+        console.log(error)
+        res.status(400).send({msg:"can't able to delete the user details"})
+    }
+} 
--- routers/registerRouter.js
+++ routers/registerRouter.js
@@ -2,5 +2,10 @@ const express = require('express');
 const register = require("../modules/registerModule");
 
 const router = express.Router();
+
 router.post('/signUp',register.signup);
+router.get('/users',register.getUsers);
+router.put('/update/:id',register.updateUser);
+router.delete('/delete/:id',register.deleteUser);
+
 module.exports= router;
